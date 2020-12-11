6e2d809d0c202e63243291ec73e2fd93839ec9ca
ElenaVlaicu <vlaicu.alexandra17@yahoo.com>
2020-12-11 23:07:19 +0200

    Am adaugat urmatoarele apiuri:  addUserAsTester si addUserToProject

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 import {app, router} from "./init/serverInit.js"
-import {createUser, listUsers, addUserToTeam, createProject} from "./operations/dboperations.js"
+import {createUser, listUsers, addUserToTeam, createProject, addUserToProject, addUserAsTester} from "./operations/dboperations.js"
 
 
 router.route("/users").post((req, resp) => {
@@ -24,6 +24,27 @@ router.route("/teams/:teamId/projects").post((req, resp) => {
     let body = req.body
     createProject(body.name, body.repo, req.params.teamId, body.userIds).then((result => resp.json(result)));
 });
+
+
+router.route("/users/:userId/projects/:projectId").post((req, resp) => {
+    addUserToProject(req.params.projectId, req.params.userId).then((result => resp.json(result))).catch(err => {
+        console.log(err)
+    })
+});
+
+router.route("/project/:projectId/users/:userId").post((req, resp) => {
+    addUserAsTester(req.params.projectId, req.params.userId).then((result => resp.json(result))).catch(err => {
+        console.log(err)
+    })
+});
+
+
+
+
+
+
+
+
 
 var port = 8000;
 app.listen(port, ()=> console.log("server is listening ..."));
