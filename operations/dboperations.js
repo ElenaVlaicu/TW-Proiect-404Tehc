f6206d5e9e0609184cd8e392fd10ae064b3ebbbc
Vlaicu Elena <vlaicu.alexandra17@yahoo.com>
2020-12-13 18:28:14 +0200

    Am adaugat verificari pentru toate api-urile disponivile

--- operations/dboperations.js
+++ operations/dboperations.js
@@ -1,9 +1,19 @@
 import sql from "mssql"
+import pkg from "sequelize";
+const { Op } = pkg;
 import { User, seqelize, Team, TeamUser, Project, ProjectUser, Bug, statusEnum, priorityEnum, severityEnum } from "../sequelize/sequelize.js"
 
 
 export async function createUser(email, password, firstName, lastName) {
     try {
+        const existingUserWithEmail = await User.findOne({
+            where: {
+                email: email
+            }
+        })
+        if (existingUserWithEmail != null){
+            throw Error("An user with this email already exists")
+        }
         const user = await User.create({
             email: email,
             password: password,
@@ -40,6 +50,16 @@ export async function listUsers(teamId) {
 
 export async function addUserToTeam(teamId, userId) {
     try {
+        const userAlreadyInTeam = await TeamUser.findOne({
+            where:{
+                teamId: teamId,
+                userId: userId
+            }
+        })
+        if (userAlreadyInTeam != null){
+            throw Error("This user is already in this team")
+        }
+        await 
         await TeamUser.create({
             teamId: teamId,
             userId: userId
@@ -53,14 +73,41 @@ export async function addUserToTeam(teamId, userId) {
 
 export async function createProject(name, repo, teamId, userIds) {
     try {
+        const alreadyProjectWithName = await Project.findOne({
+            where:{
+                name: name,
+                teamId: teamId
+            }
+        })
+        if (alreadyProjectWithName != null){
+            throw Error("There already is a project with this name in this team")
+        }
         const project = await Project.create({
             name: name,
             repo: repo,
             teamId: teamId
         });
+        if (userIds == null){
+            userIds = []
+        }
+        const existingUsers = await User.findAll({
+            where:{
+                id: {
+                    [Op.in]: userIds
+                }
+            }
+        })
+        const existingUsers2 = []
+        existingUsers.forEach(user => {
+            existingUsers2.push(user.id)
+        })
+        console.log(`qwe ${existingUsers2[0]}`)
         console.log("Created project");
         if (userIds != null) {
             userIds.forEach(userId => {
+                if (!existingUsers2.includes(userId)){
+                    throw Error(`Invalid user id: ${userId}`)
+                }
                 ProjectUser.create({
                     projectId: project.id,
                     userId: userId,
@@ -80,6 +127,19 @@ export async function addUserToProject(projectId, userId) {
     try {
         const project = await Project.findByPk(projectId);
 
+        const existingUsersInTeam = await TeamUser.findAll({
+            where:{
+                teamId: project.teamId
+            }
+        })
+        const existingUsersInTeam2 = []
+        existingUsersInTeam.forEach(teamUser => {
+            existingUsersInTeam2.push(teamUser.userId)
+        })
+        if (!existingUsersInTeam2.includes(parseInt(userId))){
+            throw Error("This user is not in the project's team")
+        }
+
         ProjectUser.create({
             projectId: project.id,
             userId: userId,
@@ -97,6 +157,19 @@ export async function addUserAsTester(projectId, userId) {
     try {
         const project = await Project.findByPk(projectId);
 
+        const existingUsersInTeam = await TeamUser.findAll({
+            where:{
+                teamId: project.teamId
+            }
+        })
+        const existingUsersInTeam2 = []
+        existingUsersInTeam.forEach(teamUser => {
+            existingUsersInTeam2.push(teamUser.userId)
+        })
+        if (existingUsersInTeam2.includes(parseInt(userId))){
+            throw Error("This user is already a member in the project's team")
+        }
+
         ProjectUser.create({
             projectId: project.id,
             userId: userId,
@@ -112,6 +185,14 @@ export async function addUserAsTester(projectId, userId) {
 export async function addBugToProject(severity, priority, description, projectId) {
     try {
         const project = await Project.findByPk(projectId);
+
+        if (!Object.keys(severityEnum).includes(severity)){
+            throw Error("Invalid severity selected")
+        }
+
+        if (!Object.keys(priorityEnum).includes(priority)){
+            throw Error("Invalid priority selected")
+        }
 
         const bug = Bug.create({
             projectId: project.id,
@@ -131,6 +212,17 @@ export async function addBugToProject(severity, priority, description, projectId
 export async function assignBugToUser(userId, bugId) {
     try {
         const currentBug = await Bug.findByPk(bugId)
+        const projectUser = await ProjectUser.findOne({
+            where: {
+                isTester: false,
+                userId: userId
+            }
+        })
+
+        if (projectUser == null){
+            throw Error("Selected user is not in the project's team")
+        }
+
         if (currentBug.userId != null) {
             throw Error("Bug is already assigned")
         }
@@ -147,6 +239,7 @@ export async function assignBugToUser(userId, bugId) {
 
 export async function setBugIsFixed(bugId, commit) {
     try{
+        //TODO: check that the currentUser is the assiged user from the selected bug
         const currentBug = await Bug.findByPk(bugId)
         if (currentBug.status != statusEnum.inProgress){
             throw Error("Cannot resolve a bug that is not assigned")
