6769066f82b6ab23c1961686d829429ab97f83a6
Vlaicu Elena <vlaicu.alexandra17@yahoo.com>
2020-12-18 18:48:19 +0200

    Corectare api-uri de delete

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
-import {app, router} from "./init/serverInit.js"
-import {createUser, listUsers, addUserToTeam, createProject, addUserToProject, addUserAsTester, addBugToProject, assignBugToUser, setBugIsFixed} from "./operations/dboperations.js"
+import { app, router } from "./init/serverInit.js"
+import { createUser, listUsers, addUserToTeam, createProject, addUserToProject, addUserAsTester, addBugToProject, assignBugToUser, setBugIsFixed } from "./operations/dboperations.js"
 import { Project, Team, TeamUser } from "./sequelize/sequelize.js";
 
 //creaza un user
@@ -7,9 +7,11 @@ router.route("/users").post((req, resp) => {
     let userData = req.body
     createUser(
         userData.email,
-         userData.password,
-          userData.firstName,
-           userData.lastName).then((result => resp.json(result)));
+        userData.password,
+        userData.firstName,
+        userData.lastName)
+        .then((result => resp.json(result)))
+        .catch((err) => { resp.status(400).json({ message: err.message }) });
 });
 
 //afiseaza toti userii unei echipe
@@ -20,67 +22,83 @@ router.route("/users").get((req, resp) => {
 //se adauga un user intr-o echipa
 router.route("/teams/users").post((req, resp) => {
     let body = req.body
-    addUserToTeam(body.teamId, body.userId).then((result => resp.json(result)));
+    addUserToTeam(body.teamId, body.userId)
+        .then((result => resp.json({ message: 'User added to team' })))
+        .catch((err) => { resp.status(400).json({ message: err.message }) });
 });
 
 //se creaza un proiect pentru o echipa (cu id-ul specificat)
 router.route("/teams/:teamId/projects").post((req, resp) => {
     let body = req.body
-    createProject(body.name, body.repo, req.params.teamId, body.userIds).then((result => resp.json(result)));
+    createProject(body.name, body.repo, req.params.teamId, body.userIds)
+        .then((result => resp.json(result)))
+        .catch((err) => { resp.status(400).json({ message: err.message }) });
 });
 
 //se adauga un user la un proiect
 router.route("/users/:userId/projects/:projectId").post((req, resp) => {
-    addUserToProject(req.params.projectId, req.params.userId).then((result => resp.json(result))).catch(err => {
-        console.log(err)
-    })
+    addUserToProject(req.params.projectId, req.params.userId)
+        .then((result => resp.json('User added to project')))
+        .catch((err) => { resp.status(400).json({ message: err.message }) })
 });
 
 //un utilizator este inregistrat ca tester
 router.route("/project/:projectId/users/:userId").post((req, resp) => {
-    addUserAsTester(req.params.projectId, req.params.userId).then((result => resp.json(result))).catch(err => {
-        console.log(err)
-    })
+    addUserAsTester(req.params.projectId, req.params.userId)
+        .then((result => resp.json('Tester added to project')))
+        .catch((err) => { resp.status(400).json({ message: err.message }) })
 });
 
 //se adauga un bug la un proiect (DOAR de catre testeri)
-router.route("/projects/:projectId/bugs").post((req,resp)=>{
-    let body=req.body
-    addBugToProject(body.severity,body.priority,body.description,req.params.projectId).then((result=>resp.json(result))).catch(err =>
-        {
-            console.log(err)
-        })
+router.route("/projects/:projectId/bugs").post((req, resp) => {
+    let body = req.body
+    addBugToProject(body.severity, body.priority, body.description, req.params.projectId)
+        .then((result => resp.json(result)))
+        .catch((err) => { resp.status(400).json({ message: err.message }) })
 });
 
 //un user (DOAR membru al unei echipe) isi asigneaza un bug
-router.route("/bugs/:bugId/users/:userId").put((req,resp)=>{
-    assignBugToUser(req.params.userId,req.params.bugId).then((result)=>resp.json(result)).catch(err =>{
-        console.log(err)
-    })
+router.route("/bugs/:bugId/users/:userId").put((req, resp) => {
+    assignBugToUser(req.params.userId, req.params.bugId)
+        .then((result) => resp.json(result))
+        .catch((err) => { resp.status(400).json({ message: err.message }) })
 });
 
 //se updateaza starea bug-ului, care devine acum rezolvat
-router.route("/bugs/:bugId/resolve").put((req, res)=>{
-    setBugIsFixed(req.params.bugId, req.body.commit).then((result)=>res.json(result)).catch(err =>{
-        console.log(err)
-    })
+router.route("/bugs/:bugId/resolve").put((req, res) => {
+    setBugIsFixed(req.params.bugId, req.body.commit)
+        .then((result) => res.json(result))
+        .catch((err) => { res.status(400).json({ message: err.message }) })
 });
 
-router.route("/project/:projectId").delete((req,res)=>{
+
+//sterge un proiect cu id-ul specificat
+router.route("/project/:projectId").delete((req, res) => {
     Project.findByPk(req.params.projectId).then(record => {
-        record.destroy();
-    }).then(() => res.json(`The project with the id ${req.params.projectId} was deleted successfully.`))
-    .catch(err => {console.log(err)})
+        if (record !== null)
+            record.destroy();
+        else { throw Error("This project does not exist") }
+    })
+
+        .then(() => res.json(`The project with the id ${req.params.projectId} was deleted successfully.`))
+        .catch((err) => { res.status(400).json({ message: err.message }) });
 });
 
-router.route("/users/:userId/teams/:teamId").delete((req,res)=>{
-    TeamUser.destroy({where: {
-        teamId: req.params.teamId,
-        userId: req.params.userId
-    }}).then(() => res.json(`The user from the team with id ${req.params.teamId} was deleted successfully.`))
-    .catch(err => {console.log(err)})
+//sterge un user dintr-o anumita echipa
+router.route("/users/:userId/teams/:teamId").delete((req, res) => {
+    TeamUser.findOne({
+        where: {
+            teamId: req.params.teamId,
+            userId: req.params.userId
+        }
+    }).then(teamUser => {
+        if (teamUser !== null) { teamUser.destroy() }
+        else {  throw Error("This user does not exist")}
+    })
+      .then(()=> res.json(`The user was deleted successfully.`))
+      .catch((err) => { res.status(400).json({ message: err.message }) });
 })
 
 
 var port = 8000;
-app.listen(port, ()=> console.log("server is listening ..."));
+app.listen(port, () => console.log("server is listening ..."));
